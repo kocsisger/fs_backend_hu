46da790af28be8a92a61a4d8591b59e0b8d076cb
Teszt Elek <you@inf.unideb.hu>
2024-12-05 12:17:26 +0100

    Initial commit

---
+++ src/main/java/hu/unideb/inf/fs_backend/FsBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.fs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsBackendApplication.class, args);
+	}
+
+}
